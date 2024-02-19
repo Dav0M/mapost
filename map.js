1991e33c4bb216d9b190718d254907f041b81469
Jimmy Xiao <xiao0261>
2024-02-19 01:33:47 -0600

    add search bar

--- map.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-    const origin = {lat: 44.9727, lng: -93.23540000000003};
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 18,
-      center: origin,
-    });
-  
-    new ClickEventHandler(map, origin);
-  }
