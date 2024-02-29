d4b1938a0186c0740e034813e6d4b6f6ccde1b99
cheng861 <zjwwen78@gmail.com>
2024-02-28 21:07:26 -0600

    edited user home and delete function

--- static/js/delete.js
+++ static/js/delete.js
@@ -1,14 +1,12 @@
 window.addEventListener("load", () => {
     delbuttons = document.querySelectorAll(".delete-button");
     for (let i = 0; i < delbuttons.length; i++) {
-        delbuttons[i].addEventListener("click", () => deletePost(delbuttons[i].dataset.postId));
+        delbuttons[i].addEventListener("click", () =>  deleteConfirmation(delbuttons[i].dataset.postId));
     }
 });
 
 async function deletePost(postId) {
-
     let data = {"id": postId};
-
     const response = await fetch("/api/post/delete", {
         method: "DELETE",
         headers: {"Content-Type": "application/json",},
@@ -17,5 +15,12 @@ async function deletePost(postId) {
 
     if (response.ok || response.status == 404) {
         location.reload();
+    }
+}
+
+function deleteConfirmation(postId) {
+    const confirmed = window.confirm("Are you sure you want to delete this post?");
+    if (confirmed) {
+        deletePost(postId);
     }
 }
